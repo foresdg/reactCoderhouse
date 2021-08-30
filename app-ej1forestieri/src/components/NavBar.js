2f046eea6354d499658b53b933fdf5969dd5b462
Agustin Forestieri <foresdg@gmail.com>
2021-08-30 19:10:21 -0300

    "desafio 2 - React coderhouse"

--- app-ej1forestieri/src/components/NavBar.js
+++ app-ej1forestieri/src/components/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
     return (
     <nav className="menu">
        <ul>
-         <li><a href="index.html"><img src={logo} alt="logoAplicacion" /></a></li>
+       <li><a href="index.html" className="logoImagen"><img src={logo} alt="logoAplicacion" /></a></li>
          <li><a href="#">Inicio</a></li>
          <li><a href="#">Sobre nosotros</a></li>
          <li><a href="#">Contacto</a></li>
